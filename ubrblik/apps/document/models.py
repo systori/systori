0b467a80a0e24fb161c7c32a6524fc5be32bd2e9
Lex Berezhny <lex@damoti.com>
2014-12-03 16:37:51 -0500

    added some text on how document would work

--- ubrblik/apps/document/models.py
+++ ubrblik/apps/document/models.py
@@ -1,6 +1,29 @@
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 
+"""
+Document represents a work-in-progress model for a physical document.
+
+Workflow is something like this:
+
+1. User creates a new Document object (either Invoice, Proposal, Report, etc).
+
+2. They will associate Tasks with this document. At this point the tasks might
+   change due to other factors in the system (workers marking tasks as done).
+
+3. When user is satisfied with the document and have verified that the tasks
+   look correct they must generate a PDF.
+
+4. Once a PDF is generated they can no longer edit the document. They can easily
+   duplicate the document and start a new one.
+
+Rationale:
+
+Because a Document represents references to live Tasks (which can change due to other
+factors) it would be confusing to leave a document editable after it has been verified
+and generated. By locking editing and generating a PDF we have created a snap shot at a
+point in time where the user was satisfied with the state of all associated Tasks.
+"""
 
 class Document(models.Model):
     INVOICE = "invoice"
