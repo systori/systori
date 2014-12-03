ddfdd86e7d8b2f1adad49a02d138725328003a84
Lex Berezhny <lex@damoti.com>
2014-12-02 22:27:00 -0500

    generated translation strings

--- ubrblik/settings/common.py
+++ ubrblik/settings/common.py
@@ -90,6 +90,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/dev/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = ''
+MEDIA_ROOT = ''
 
 STATICFILES_DIRS = (
     ('js', 'ubrblik/static/js'),
