cbfa299cf85609830f3b6ea37bc1ba99ec426d40
Lex Berezhny <lex@damoti.com>
2014-12-15 22:31:55 -0500

    finally made it through the cross site scripting stuff

--- ubrblik/apps/project/api.py
+++ ubrblik/apps/project/api.py
@@ -1,10 +1,11 @@
+from django.http.response import HttpResponse
 from django.core.exceptions import ValidationError
 from django.forms.models import model_to_dict
 from django.conf.urls import url
 from tastypie import fields
 from tastypie import http
 from tastypie.resources import ModelResource
-from tastypie.authentication import SessionAuthentication
+from tastypie.authentication import SessionAuthentication, BasicAuthentication, MultiAuthentication
 from tastypie.authorization import Authorization
 from tastypie.exceptions import ImmediateHttpResponse
 from tastypie.utils import trailing_slash
@@ -12,11 +13,41 @@ from .models import Project
 
 
 class BaseMeta:
-    authentication = SessionAuthentication()
+    authentication = MultiAuthentication(BasicAuthentication(), SessionAuthentication())
     authorization = Authorization()
 
+class BaseCorsResource(ModelResource):
+    """
+    Class implementing CORS
+    """
+    def create_response(self, *args, **kwargs):
+        response = super(BaseCorsResource, self).create_response(*args, **kwargs)
+        response['Access-Control-Allow-Origin'] = '*'
+        response['Access-Control-Allow-Headers'] = 'Content-Type'
+        return response
 
-class ProjectResource(ModelResource):
+    def method_check(self, request, allowed=None):
+        if allowed is None:
+            allowed = []
+
+        request_method = request.method.lower()
+        allows = ','.join(map(str.upper, allowed))
+
+        if request_method == 'options':
+            response = HttpResponse(allows)
+            response['Access-Control-Allow-Origin'] = '*'
+            response['Access-Control-Allow-Headers'] = 'Content-Type, Authorization'
+            response['Allow'] = allows
+            raise ImmediateHttpResponse(response=response)
+
+        if not request_method in allowed:
+            response = http.HttpMethodNotAllowed(allows)
+            response['Allow'] = allows
+            raise ImmediateHttpResponse(response=response)
+
+        return request_method
+
+class ProjectResource(BaseCorsResource):
     class Meta(BaseMeta):
         queryset = Project.objects.all()
         resource_name = 'project'
