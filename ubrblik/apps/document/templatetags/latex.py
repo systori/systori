6b49065af6a0b572a3c8e9c53a4f1ea09ddb742a
Marius Räsener <m.raesener@gmail.com>
2015-02-03 04:55:54 -0800

    added tex character escaping, attention |safe filter used

--- ubrblik/apps/document/templatetags/latex.py
+++ ubrblik/apps/document/templatetags/latex.py
@@ -1,9 +1,39 @@
 from django.template.defaultfilters import stringfilter
 from django import template
+import re
 register = template.Library()
 
 @register.filter
 @stringfilter
-def bold(value):
-     return "\\textbf{{{}}}".format(value)
- 
+def bold(text):
+    """
+        :param text: a plain text message
+        :return: the message returned to appear with Bold text styling in LaTeX
+    """
+    return "\\textbf{{{}}}".format(text)
+
+@register.filter
+@stringfilter
+def tex_escape(text):
+    """
+        :param text: a plain text message
+        :return: the message escaped to appear correctly in LaTeX
+    """
+    
+    conv = {
+        '&': r'\&',
+        '%': r'\%',
+        '$': r'\$',
+        '#': r'\#',
+        '_': r'\_',
+        '{': r'\{',
+        '}': r'\}',
+        '~': r'\textasciitilde{}',
+        '^': r'\^{}',
+        '\\': r'\textbackslash{}',
+        '<': r'\textless',
+        '>': r'\textgreater',
+    }
+    conv = dict((re.escape(k), v) for k, v in conv.items())
+    pattern = re.compile("|".join(conv.keys()))
+    return pattern.sub(lambda m: conv[re.escape(m.group(0))], text)
