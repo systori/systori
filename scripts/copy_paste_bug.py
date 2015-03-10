d987038b4cfcfd36e8273850eb7ea730319d12ba
Marius Räsener <m.raesener@gmail.com>
2015-03-10 02:42:10 -0700

    actually working bug fix script copy and paste from mac

--- scripts/copy_paste_bug.py
+++ scripts/copy_paste_bug.py
@@ -1,5 +1,6 @@
 import os
 import re
+import unicodedata
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ubrblik.settings")
 import django
 django.setup()
@@ -16,12 +17,11 @@ for job in p22.jobs.all():
         name_striped = taskgroup.name.strip()
         description_striped = taskgroup.description.strip()
         taskgroup.name = re.sub('\s+',' ',name_striped)
+        taskgroup.name = unicodedata.normalize('NFC', taskgroup.name)
         taskgroup.description = re.sub('\s+',' ',description_striped)
+        taskgroup.description = unicodedata.normalize('NFC', taskgroup.description)
         print("after name: {}".format(taskgroup.name))
         print("after description: {}".format(taskgroup.description))
-        for c in taskgroup.name:
-            if(ord(c)==776):
-                task.name = task.name.replace(c,"")
         taskgroup.save()
         for task in taskgroup.tasks.all():
             print("before name: {}".format(task.name))
@@ -29,17 +29,11 @@ for job in p22.jobs.all():
             name_striped = task.name.strip()
             description_striped = task.description.strip()
             task.name = re.sub('\s+',' ',name_striped)
+            task.name = unicodedata.normalize('NFC', task.name)
             task.description = re.sub('\s+',' ',description_striped)
+            task.description = unicodedata.normalize('NFC', task.description)
             print("after name: {}".format(task.name))
             print("after description: {}".format(task.description))
-            for c in task.name:
-                if(ord(c)==776):
-                    print("{}->{}".format(c, ord(c)))
-                    task.name = task.name.replace(c,"")
-            for c in task.description:
-                if(ord(c)==776):
-                    print("{}->{}".format(c, ord(c)))
-                    task.description = task.description.replace(c,"")
             print("\n\n\n")
             task.save()
 
