2a8b46f457879b70ae3d5c12439a451a9fbf3867
Lex Berezhny <lex@damoti.com>
2015-03-16 14:58:53 -0400

    added link to open field app within desktop app

--- systori/apps/field/urls.py
+++ systori/apps/field/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import patterns, url
 from ..user.authorization import field_auth
 from .views import FieldProjectList, FieldProjectView, FieldJobView, FieldTaskView
-from .views import FieldAddSelfToJobSite
+from .views import FieldDashboard, FieldAddSelfToJobSite
 
 urlpatterns = patterns('',
   url(r'^project-(?P<project_pk>\d+)/job-(?P<job_pk>\d+)/task-(?P<task_pk>\d+)/$', field_auth(FieldTaskView.as_view()), name='field.task'),
@@ -9,4 +9,6 @@ urlpatterns = patterns('',
   url(r'^project-(?P<project_pk>\d+)/$', field_auth(FieldProjectView.as_view()), name='field.project'),
   url(r'^jobsite-(?P<pk>\d+)/add-self-as-(?P<role>(foreman|laborer))$', field_auth(FieldAddSelfToJobSite.as_view()), name='field.add_self_to_jobsite'),
   url(r'^projects$', field_auth(FieldProjectList.as_view()), name='field.projects'),
+
+  url(r'^$', field_auth(FieldDashboard.as_view()), name='field.dashboard'),
 )
