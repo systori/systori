a12ae6fe4e64b80ce9bad60809f234c481b21847
Lex Berezhny <lex@damoti.com>
2015-04-21 22:01:45 -0400

    adding migration script for phases :-)

--- systori/apps/project/migrations/0010_auto_20150422_0248.py
+++ systori/apps/project/migrations/0010_auto_20150422_0248.py
@@ -5,6 +5,24 @@ from django.db import models, migrations
 import django_fsm
 
 
+def update_project_phase(apps, schema):
+    Project = apps.get_model('project', 'Project')
+    Job = apps.get_model('task', 'Job')
+    Proposal = apps.get_model('document', 'Proposal')
+    for project in Project.objects.filter(is_template=False).all():
+        if Job.objects.filter(project_id=project.id, status='started').exists():
+            print('executing')
+            project.phase = 'executing'
+            project.save()
+        elif Proposal.objects.filter(project_id=project.id, status='approved').exists():
+            print('planning')
+            project.phase = 'planning'
+            project.save()
+        elif Proposal.objects.filter(project_id=project.id).exists():
+            print('tendering')
+            project.phase = 'tendering'
+            project.save()
+
 class Migration(migrations.Migration):
 
     dependencies = [
@@ -15,11 +33,12 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='project',
             name='phase',
-            field=django_fsm.FSMField(max_length=50, choices=[('prospective', 'Prospective'), ('tendering', 'Tendering'), ('planning', 'Planning'), ('executing', 'Executing'), ('settlement', 'Settlement'), ('warranty', 'Warranty'), ('finished', 'Finished')], default='prospective'),
+            field=django_fsm.FSMField(default='prospective', max_length=50, choices=[('prospective', 'Prospective'), ('tendering', 'Tendering'), ('planning', 'Planning'), ('executing', 'Executing'), ('settlement', 'Settlement'), ('warranty', 'Warranty'), ('finished', 'Finished')]),
         ),
         migrations.AddField(
             model_name='project',
             name='state',
-            field=django_fsm.FSMField(max_length=50, choices=[('active', 'Active'), ('paused', 'Paused'), ('canceled', 'Canceled'), ('disputed', 'Disputed'), ('stopped', 'Stopped')], default='active'),
+            field=django_fsm.FSMField(default='active', max_length=50, choices=[('active', 'Active'), ('paused', 'Paused'), ('canceled', 'Canceled'), ('disputed', 'Disputed'), ('stopped', 'Stopped')]),
         ),
+        migrations.RunPython(update_project_phase)
     ]
