4b9528fda4e2a41d67e9c2bc805f5c91edf2aa08
Lex Berezhny <lex@damoti.com>
2015-07-06 23:48:52 -0400

    user manager wip

--- systori/apps/field/middleware.py
+++ systori/apps/field/middleware.py
@@ -4,6 +4,9 @@ from ..project.models import JobSite, DailyPlan
 
 class FieldMiddleware:
     def process_view(self, request, view, args, kwargs):
+        if not request.company:
+            return
+
         if 'jobsite_pk' in kwargs:
             request.jobsite = JobSite.objects.get(pk=kwargs['jobsite_pk'])
             if 'dailyplan_url_id' in kwargs:
@@ -17,7 +20,7 @@ class FieldMiddleware:
             request.selected_day = date(*map(int, kwargs['selected_day'].split('-')))
 
     def process_template_response(self, request, response):
-        if hasattr(response, 'context_data'):
+        if request.company and hasattr(response, 'context_data'):
             if hasattr(request, 'jobsite') and 'jobsite' not in response.context_data:
                 response.context_data['jobsite'] = request.jobsite
             if hasattr(request, 'dailyplan') and 'dailyplan' not in response.context_data:
