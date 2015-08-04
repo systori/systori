624b7b3510e8c8fb5cc72cd5825ab689e6a1aeb7
Lex Berezhny <lex@damoti.com>
2015-08-04 14:20:48 -0400

    updated p83 parser

--- scripts/p83_parser.py
+++ scripts/p83_parser.py
@@ -19,30 +19,48 @@ Beschreibung = tag_compiler('Beschreibung')
 # Keep returning attributes until we find a # which
 # means some other tag has started.
 ATTR = re.compile('\[(?P<key>\w+)\](?P<value>.*?)\[end\]\s*(?P<is_last>\#)?', re.DOTALL)
-def get_attributes(contents):
-    for attr in ATTR.finditer(contents):
+def get_attributes(content):
+    for attr in ATTR.finditer(content):
         yield attr.group('key'), attr.group('value')
         if attr.group('is_last'): break
 
-def attr_dict(contents):
-    return dict(list(get_attributes(contents)))
+def attr_dict(content):
+    return dict(list(get_attributes(content)))
 
+def get_content_attrs(match):
+    content = match.group('content')
+    attrs = attr_dict(content)
+    attrs.update(attr_dict(Beschreibung.search(content).group('content')))
+    return content, attrs
 
 # Build up the structure and convert it into something
 # Systori can use...
-
-def descend(contents):
-    for level in LVBereich.finditer(contents):
-        inner = level.group('content')
-        for attr in get_attributes(inner):
-            print(attr)
-        descend(inner)
 
 data = open('gaeb2000_file.p83', encoding='latin_1').read()
 content = LV.search(data).group('content')
 project = {
     'attrs': attr_dict(LVInfo.search(content).group('content')),
-    'levels': descend(content)
+    'jobs': []
 }
 
-print(project['attrs'])
+for job in LVBereich.finditer(content):
+    job_content, job_attrs = get_content_attrs(job)
+    print('Job:', job_attrs['Kurztext'])
+    print()
+
+    for taskgroup in LVBereich.finditer(job_content):
+        taskgroup_content, taskgroup_attrs = get_content_attrs(taskgroup)
+        print()
+        print(' Task Group:', taskgroup_attrs['Kurztext'])
+        for key, value in taskgroup_attrs.items():
+            if key in ['Langtext', 'Kurztext', 'Bez']: continue
+            print('  {}: {}'.format(key, value))
+        print()
+
+        for task in Position.finditer(taskgroup_content):
+            task_content, task_attrs = get_content_attrs(task)
+            print('   Task:', task_attrs['Kurztext'])
+            for key, value in task_attrs.items():
+                if key in ['Langtext', 'Kurztext']: continue
+                print('     {}: {}'.format(key, value))
+            print()
