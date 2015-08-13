49b3e3adcb7030f016c7adc94726b797455669b3
Marius Räsener <m.raesener@gmail.com>
2015-08-13 16:18:32 +0200

    little optimizations

--- systori/apps/document/type/itemized_listing.py
+++ systori/apps/document/type/itemized_listing.py
@@ -3,83 +3,16 @@ from datetime import date
 
 from reportlab.lib.units import mm
 from reportlab.platypus import Paragraph, Spacer
-from reportlab.lib import colors
 
 from django.utils.formats import date_format
 from django.utils.translation import ugettext as _
 
-from systori.lib.templatetags.customformatting import ubrdecimal, money
-
-from .style import SystoriDocumentWithoutFirstPage, stylesheet, force_break, ContinuationTable, TableFormatter, p, b
+from .style import SystoriDocumentWithoutFirstPage, stylesheet
 from .style import PortraitStationaryCanvasWithoutFirstPage
-#from .invoice import collate_tasks, collate_tasks_total
-from . import font
+from .invoice import collate_tasks, collate_tasks_total
 
 DEBUG_DOCUMENT = False  # Shows boxes in rendered output
 
-def collate_tasks(invoice, available_width):
-
-    t = TableFormatter([1, 0, 1, 1, 1, 1], available_width, debug=DEBUG_DOCUMENT)
-    t.style.append(('LEFTPADDING', (0, 0), (-1, -1), 0))
-    t.style.append(('RIGHTPADDING', (-1, 0), (-1, -1), 0))
-    t.style.append(('VALIGN', (0, 0), (-1, -1), 'TOP'))
-    t.style.append(('LINEABOVE', (0, 'splitfirst'), (-1, 'splitfirst'), 0.25, colors.black))
-
-    t.row(_("Pos."), _("Description"), _("Amount"), '', _("Price"), _("Total"))
-    t.row_style('FONTNAME', 0, -1, font.bold)
-    t.row_style('ALIGNMENT', 2, 3, "CENTER")
-    t.row_style('ALIGNMENT', 4, -1, "RIGHT")
-    t.row_style('SPAN', 2, 3)
-
-    for job in invoice['jobs']:
-        t.row(b(job['code']), b(job['name']))
-        t.row_style('SPAN', 1, -1)
-
-        for taskgroup in job['taskgroups']:
-            t.row(b(taskgroup['code']), b(taskgroup['name']))
-            t.row_style('SPAN', 1, -1)
-
-            for task in taskgroup['tasks']:
-                t.row(p(task['code']), p(task['name']))
-                t.row_style('SPAN', 1, -2)
-
-                t.row('', '', ubrdecimal(task['complete']), p(task['unit']), money(task['price']), money(task['total']))
-                t.row_style('ALIGNMENT', 1, -1, "RIGHT")
-
-                t.row_style('BOTTOMPADDING', 0, -1, 10)
-
-                t.keep_previous_n_rows_together(2)
-
-            t.row('', b('{} {} - {}'.format(_('Total'), taskgroup['code'], taskgroup['name'])),
-                  '', '', '', money(taskgroup['total']))
-            t.row_style('FONTNAME', 0, -1, font.bold)
-            t.row_style('ALIGNMENT', -1, -1, "RIGHT")
-            t.row_style('SPAN', 1, 4)
-
-            t.row('')
-
-    return t.get_table(ContinuationTable, repeatRows=1)
-
-
-def collate_tasks_total(invoice, available_width):
-
-    t = TableFormatter([0, 1], available_width, debug=DEBUG_DOCUMENT)
-    t.style.append(('RIGHTPADDING', (-1, 0), (-1, -1), 0))
-    t.style.append(('LEFTPADDING', (0, 0), (0, -1), 0))
-    t.style.append(('FONTNAME', (0, 0), (-1, -1), font.bold))
-    t.style.append(('ALIGNMENT', (0, 0), (-1, -1), "RIGHT"))
-
-    for job in invoice['jobs']:
-        for taskgroup in job['taskgroups']:
-            t.row(b('{} {} - {}'.format(_('Total'), taskgroup['code'], taskgroup['name'])),
-                  money(taskgroup['total']))
-    t.row_style('LINEBELOW', 0, 1, 0.25, colors.black)
-
-    t.row(_("Total without VAT"), money(invoice['total_base']))
-    t.row("19,00% "+_("VAT"), money(invoice['total_tax']))
-    t.row(_("Total including VAT"), money(invoice['total_gross']))
-
-    return t.get_table()
 
 def render(project, format):
 
