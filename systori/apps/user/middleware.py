db388752c25c2c916e479f3de9470ad39eac068b
Andrey Beletsky <andrey@vr2.net>
2015-09-10 22:01:02 +0300

    Consolidate ForceDefaultLanguageMiddleware into SetLanguageMiddleware

--- systori/apps/user/middleware.py
+++ systori/apps/user/middleware.py
@@ -7,9 +7,17 @@ class SetLanguageMiddleware:
     Should be placed in MIDDLEWARE_CLASSES:
     - before django's LocaleMiddleware
     - after django's AuthenticationMiddleware
+
+    Also will force the I18N machinery to always choose settings.LANGUAGE_CODE
+    as the default initial language when HTTP_ACCEPT_LANGUAGE is set to english.
+    Rationale is that not everyone knows how to set this setting correctly in
+    their browser so we default to German.
     """
 
     def process_request(self, request):
+        if request.META.get('HTTP_ACCEPT_LANGUAGE', '').startswith('en'):
+            del request.META['HTTP_ACCEPT_LANGUAGE']
+
         if request.user.is_authenticated():
             user = request.user
             if user.language and check_for_language(user.language):
