d9496b698f86e03783843f95047f02564826c27a
Lex Berezhny <lex@damoti.com>
2015-09-30 22:35:11 -0400

    ratio based payment auto splitter

--- systori/lib/templatetags/ubrutils.py
+++ systori/lib/templatetags/ubrutils.py
@@ -7,3 +7,7 @@ register = template.Library()
 @register.simple_tag
 def random_pick(*opts):
     return opts[randint(0, len(opts) - 1)]
+
+@register.filter
+def divide(numerator, denominator):
+    return round(numerator / denominator, 4)
