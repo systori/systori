90f5f00cfaed765c51765b9a6304ad2363c481e9
Andrey Beletsky <andrey@vr2.net>
2015-11-23 22:52:17 +0800

    delete key handler on LineItem doesn't remove non-empty parents anymore

--- systori/dart/web/editor.dart
+++ systori/dart/web/editor.dart
@@ -968,9 +968,9 @@ class LineItemElement extends EditableElement {
 
     void handle_delete_key(event) {
         var saved_parent = this.parent;
-
         super.handle_delete_key(event);
-        if (saved_parent.can_delete()) {
+        
+        if (event.shiftKey && saved_parent.can_delete() && saved_parent.classes.contains('empty')) {
             saved_parent.delete();
             saved_parent.remove();
         }
