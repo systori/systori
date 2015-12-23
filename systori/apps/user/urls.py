0bc0102741ec860ee4b0d22fa159edd033b62d04
Lex Berezhny <lex@damoti.com>
2015-12-23 02:13:53 -0500

    fix for #119

--- systori/apps/user/urls.py
+++ systori/apps/user/urls.py
@@ -15,7 +15,7 @@ urlpatterns = [
     url(r'^add-user$', office_auth(views.UserAdd.as_view()), name='user.add'),
     url(r'^user-(?P<pk>\d+)$', office_auth(views.UserView.as_view()), name='user.view'),
     url(r'^user-(?P<pk>\d+)/edit$', office_auth(views.UserUpdate.as_view()), name='user.edit'),
-    url(r'^user-(?P<pk>\d+)/remove$', office_auth(views.UserRemove.as_view()), name='user.remove'),
+    url(r'^access-(?P<pk>\d+)/remove$', office_auth(views.AccessRemove.as_view()), name='access.remove'),
     url(
         r'^user-(?P<pk>\d+)/generate-password$', 
         office_auth(views.UserGeneratePassword.as_view()), name='user.generate.password'),
