8d511dec3b447884634c91ba3c660d1b3533be27
Lex Berezhny <lex@damoti.com>
2016-02-20 22:31:57 -0500

    reset migrations sql

---
+++ reset_migrations.sql
@@ -0,0 +1,7 @@
+DELETE FROM django_migrations WHERE app = 'accounting';
+DELETE FROM django_migrations WHERE app = 'company';
+DELETE FROM django_migrations WHERE app = 'directory';
+DELETE FROM django_migrations WHERE app = 'document';
+DELETE FROM django_migrations WHERE app = 'project';
+DELETE FROM django_migrations WHERE app = 'task';
+DELETE FROM django_migrations WHERE app = 'user';
