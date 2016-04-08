e1e0468a489f76768b5afd77c3377af3c33be587
Andrey Beletsky <andrey@vr2.net>
2016-04-09 01:23:52 +0700

    add subdomain supporting TestCase

---
+++ systori/lib/testing.py
@@ -0,0 +1,20 @@
+from django.test import TestCase
+from django.test.client import Client
+from django.conf import settings
+
+from ..company.factories import CompanyFactory
+
+
+class SubdomainClient(Client):
+    """
+    Enable Systori HTTP_HOST/subdomain functionality on test client requests
+    """
+    def generic(self, method, path, data='',
+                content_type='application/octet-stream', secure=False,
+                **extra):
+        extra.setdefault('HTTP_HOST', CompanyFactory.schema + '.' + settings.SERVER_NAME)
+        return super().generic(method, path, data, content_type, secure, **extra)
+
+
+class SystoriTestCase(TestCase):
+    client_class = SubdomainClient
