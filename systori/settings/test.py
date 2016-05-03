880cbed3be70e6457743d6c4132dc816a5f66c6c
Lex Berezhny <lex@damoti.com>
2016-05-03 01:47:46 -0400

    getting tests to run in shipmaster

---
+++ systori/settings/test.py
@@ -0,0 +1,29 @@
+from .common import *
+
+SERVER_NAME = 'systori.localhost'
+SESSION_COOKIE_DOMAIN = '.'+SERVER_NAME
+
+GOOGLE_MAPS_API_KEY = "AIzaSyDvwtHHJ_FrNVkbKoHoWh2r2E5PtV5rmLY"
+
+SESSION_COOKIE_SECURE = False
+CSRF_COOKIE_SECURE = False
+
+DEBUG = True
+
+DATABASES['default'].update({
+    'NAME': 'postgres',
+    'USER': 'postgres',
+    'HOST': 'db',
+    'PORT': 5432,
+    'TEST': {
+        'SERIALIZE': False
+    }
+})
+
+STATICFILES_DIRS += (
+    ('dart', os.path.join(ROOT_DIR, 'systori/dart/build/web')),
+)
+
+PASSWORD_HASHERS = (
+    'django.contrib.auth.hashers.MD5PasswordHasher',
+)
