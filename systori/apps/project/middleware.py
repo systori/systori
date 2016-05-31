c89becc0035cdbaaf50a35277f7166d50bb254d7
Lex Berezhny <lex@damoti.com>
2016-05-31 14:16:38 -0400

    only query the database for project, even template project, if the user is actually logged-in

--- systori/apps/project/middleware.py
+++ systori/apps/project/middleware.py
@@ -5,16 +5,18 @@ from .models import Project
 
 class ProjectMiddleware:
     def process_view(self, request, view, args, kwargs):
-        if request.company:
-            if 'project_pk' in kwargs:
-                request.project = Project.objects \
-                    .prefetch_related("jobsites") \
-                    .get(pk=kwargs['project_pk'])
-            else:
-                request.project = Project.objects.template().get()
+        if request.user.is_authenticated():
+            if request.company:
+                if 'project_pk' in kwargs:
+                    request.project = Project.objects \
+                        .prefetch_related("jobsites") \
+                        .get(pk=kwargs['project_pk'])
+                else:
+                    request.project = Project.objects.template().get()
 
     def process_template_response(self, request, response):
-        if request.company and getattr(response, 'context_data') is not None:
-            if 'project' not in response.context_data and hasattr(request, 'project'):
-                response.context_data['project'] = request.project
+        if request.user.is_authenticated():
+            if request.company and getattr(response, 'context_data') is not None:
+                if 'project' not in response.context_data and hasattr(request, 'project'):
+                    response.context_data['project'] = request.project
         return response
