2be2831ee4672790ace7f70a44b0feb1acf00204
Lex Berezhny <lex@damoti.com>
2016-11-19 00:14:53 -0500

    fixed decimal DifferenceFormat

--- systori/dart/lib/decimal.dart
+++ systori/dart/lib/decimal.dart
@@ -6,7 +6,7 @@ Decimal sum(Iterable<Decimal> decimals) => decimals.reduce((i, j) => i + j);
 
 makeNumberFormat() => new NumberFormat("#,###,###,##0.####");
 makeMoneyFormat() => new NumberFormat("#,###,###,##0.00#");
-makeDifferenceFormat() => new NumberFormat("+#,###,###,##0.00#");
+makeDifferenceFormat() => new NumberFormat("+#,###,###,##0.00#;-#,###,###,##0.00#");
 
 
 class Decimal implements Comparable<Decimal> {
