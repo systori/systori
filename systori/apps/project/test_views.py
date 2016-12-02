46ca9dfd4c728f64f8efaca5e21bc9981f13c345
Marius Räsener <mr@mehr-handwerk.de>
2016-12-02 14:56:05 +0100

    added test_views.py back in

---
+++ systori/apps/project/test_views.py
@@ -0,0 +1,58 @@
+from django.core.urlresolvers import reverse
+
+from systori.lib.testing import ClientTestCase
+
+from ..task.factories import JobFactory
+from .factories import ProjectFactory
+from .models import Project
+
+
+class TestProjectListView(ClientTestCase):
+
+    def test_load_without_search_term(self):
+        response = self.client.get(reverse('projects'))
+        self.assertEqual(200, response.status_code)
+
+    def test_load_expected_variables(self):
+        ProjectFactory()
+        response = self.client.get(reverse('projects'))
+        for element in ['sys-sort-button', 'sys-phase-button', 'sys-project-tile']:
+            self.assertContains(response, element)
+        for phase in dict(Project.PHASE_CHOICES).keys():
+            self.assertContains(response, phase)
+
+
+class TestProgressViews(ClientTestCase):
+
+    def test_project_progress(self):
+        project = ProjectFactory()
+        response = self.client.get(reverse('project.progress', args=[project.id]), {})
+        self.assertEqual(200, response.status_code)
+
+    def test_project_progress_all(self):
+        response = self.client.get(reverse('project.progress.all'), {})
+        self.assertEqual(200, response.status_code)
+
+
+class TestProjectViews(ClientTestCase):
+
+    def test_create_project(self):
+
+        response = self.client.get(reverse('project.create'), {})
+        self.assertEqual(200, response.status_code)
+
+        response = self.client.post(reverse('project.create'), {
+            'name': 'new test project',
+            'address': 'One Street',
+            'city': 'Town',
+            'postal_code': '12345',
+            'structure_format': '0.0',
+        })
+        self.assertEqual(302, response.status_code)
+        self.assertTrue(Project.objects.filter(name='new test project').exists())
+
+    def test_view_project(self):
+        project = ProjectFactory()
+        JobFactory(project=project)
+        response = self.client.get(reverse('project.view', args=[project.pk]), {})
+        self.assertEqual(200, response.status_code)
