f7f0d8758d36dbc0b3007656d41ec4b140b501e6
Marius Räsener <mr@mehr-handwerk.de>
2017-02-21 12:16:38 +0100

    rebased dev, added more Tests

--- systori/apps/timetracking/test_forms.py
+++ systori/apps/timetracking/test_forms.py
@@ -116,3 +116,56 @@ class ManualTimerFormTest(TestCase):
         self.assertEqual(
             self.worker.user.get_full_name(),
             list(f.fields['worker'].choices)[1][1])
+
+    def test_add_Timer_to_date_with_already_existing_Timers(self):
+        tz = timezone.get_current_timezone()
+        start = tz.localize(datetime(2016, 9, 1, 7))
+        end = start + timedelta(hours=9)
+        form = forms.ManualTimerForm(data={
+            'worker': self.worker.pk,
+            'start': start.strftime('%d.%m.%Y %H:%M'),
+            'end': end.strftime('%d.%m.%Y %H:%M'),
+            'kind': Timer.WORK,
+            'morning_break': True,
+            'lunch_break': True
+        }, company=self.company)
+        self.assertTrue(form.is_valid())
+        form.save()
+        timer1, timer2, timer3 = (timer for timer in Timer.objects.all())
+        self.assertEqual(timer1.duration, 60 * 60 * 2)
+
+        start = tz.localize(datetime(2016, 9, 1, 17))
+        end = start + timedelta(hours=2)
+        form = forms.ManualTimerForm(data={
+            'worker': self.worker.pk,
+            'start': start.strftime('%d.%m.%Y %H:%M'),
+            'end': end.strftime('%d.%m.%Y %H:%M'),
+            'kind': Timer.WORK
+        }, company=self.company)
+        self.assertTrue(form.is_valid())
+        form.save()
+        self.assertEqual(len(Timer.objects.all()), 4)
+
+    def test_save_and_check_overlap(self):
+        tz = timezone.get_current_timezone()
+        start = tz.localize(datetime(2016, 9, 1, 7))
+        end = start + timedelta(hours=9)
+        form = forms.ManualTimerForm(data={
+            'worker': self.worker.pk,
+            'start': start.strftime('%d.%m.%Y %H:%M'),
+            'end': end.strftime('%d.%m.%Y %H:%M'),
+            'kind': Timer.WORK
+        }, company=self.company)
+        self.assertTrue(form.is_valid())
+        timer = form.save()[0]
+        self.assertEqual(timer.duration, 60 * 60 * 9)
+
+        start = tz.localize(datetime(2016, 9, 1, 8))
+        end = start + timedelta(hours=2)
+        form = forms.ManualTimerForm(data={
+            'worker': self.worker.pk,
+            'start': start.strftime('%d.%m.%Y %H:%M'),
+            'end': end.strftime('%d.%m.%Y %H:%M'),
+            'kind': Timer.WORK
+        }, company=self.company)
+        self.assertFalse(form.is_valid())
