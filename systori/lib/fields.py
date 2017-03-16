4ec90b7505bb76dbe80b7fa05f10f10a70ceabea
Lex Berezhny <lex@damoti.com>
2017-03-16 17:55:03 -0400

    systori.lib.models messed up unit tests, reverting it to systori.lib.fields

--- systori/lib/fields.py
+++ systori/lib/fields.py
@@ -1,5 +1,31 @@
 from django import forms
 from django.utils.functional import cached_property
+from django.utils.translation import ugettext_lazy as _
+
+
+def apply_all_kwargs(obj, **kwargs):
+    for field in obj._meta.get_fields():
+        if field.attname in kwargs:
+            value = kwargs.pop(field.attname)
+            setattr(obj, field.attname, value)
+    if kwargs:
+        raise TypeError(
+            "'{}' is not a valid field of {}"
+            .format(list(kwargs)[0], obj.__class__.__name__)
+        )
+
+
+class RateType:
+    HOURLY = 'hourly'
+    DAILY = 'daily'
+    WEEKLY = 'weekly'
+    FLAT = 'flat'
+    RATE_CHOICES = [
+        (HOURLY, _("Hourly")),
+        (DAILY, _("Daily")),
+        (WEEKLY, _("Weekly")),
+        (FLAT, _("Flat Fee")),
+    ]
 
 
 class DecimalMinuteHoursField(forms.DecimalField):
