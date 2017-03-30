b0ed07480afd7e1e4368f4f40e0d35fff37ce22e
Marius Räsener <mr@mehr-handwerk.de>
2017-03-30 22:50:51 +0200

    working version, not tested yet

---
+++ systori/apps/project/api.py
@@ -0,0 +1,19 @@
+from django.conf.urls import url
+from rest_framework import views
+from rest_framework import response
+from ..user.permissions import HasStaffAccess
+from .models import Project
+
+
+class ProjectAvailableAPI(views.APIView):
+
+    permission_classes = (HasStaffAccess,)
+
+    def get(self, request, *args, **kwargs):
+        pk = int(kwargs['pk'])
+        return response.Response(Project.objects.filter(pk=pk).exists())
+
+
+urlpatterns = [
+    url(r'^project-available/(?P<pk>\d+)$', ProjectAvailableAPI.as_view(), name='api.project.available'),
+]
---
+++ systori/dart/web/goto_project.dart
@@ -0,0 +1,58 @@
+import 'dart:html';
+import 'package:systori/src/inputs/input.dart';
+
+
+class SystoriGotoProjectInput extends TextInput with KeyboardHandler {
+    factory SystoriGotoProjectInput() => new Element.tag(tag);
+
+    static final tag = 'sys-goto-project';
+    static final project_url = window.location.origin+"/project-";
+    bool exists;
+    int project_id;
+
+    SystoriGotoProjectInput.created() : super.created() {
+        addKeyHandler(this);
+    }
+
+    void checkProjectExists(project_id) {
+        String url = window.location.origin+"/api/project-available/"+project_id.toString();
+        HttpRequest.getString(url).then(parseResponse);
+    }
+
+    void parseResponse(String responseText) {
+        if (responseText == 'true') {
+            exists = true;
+        } else {
+            exists = false;
+            this.style.color = "red";
+        }
+    }
+
+    @override
+    onKeyUpEvent(KeyEvent e, TextInput input) {
+        input.style.color = "initial";
+        try {
+            project_id = int.parse(input.text);
+            checkProjectExists(project_id);
+        } catch(_) {
+            project_id = 0;
+            input.style.color = "red";
+        }
+    }
+
+    @override
+    onKeyDownEvent(KeyEvent e, TextInput input) {
+        if (e.keyCode == KeyCode.ENTER) {
+            e.preventDefault();
+            if (project_id != 0 && exists) {
+                window.location.href = project_url + project_id.toString();
+            }
+        }
+    }
+
+}
+
+
+void main() {
+    document.registerElement('sys-goto-project', SystoriGotoProjectInput);
+}
