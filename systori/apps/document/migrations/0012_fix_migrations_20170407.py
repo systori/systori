960cd0a3ed10e9908dec69acaaba2304f8164972
Marius Räsener <mr@mehr-handwerk.de>
2017-04-07 14:05:57 +0200

    migrations fix

---
+++ systori/apps/document/migrations/0012_fix_migrations_20170407.py
@@ -0,0 +1,24 @@
+from django.db import migrations
+from postgres_schema.operations import RunInSchemas
+
+
+def migrate_json(apps, schema_editor):
+    from systori.apps.document.models import Invoice, Proposal
+    for invoice in Invoice.objects.all():
+        invoice.json['project_id'] = invoice.project_id
+        invoice.json['show_project_id'] = False
+        invoice.save()
+    for proposal in Proposal.objects.all():
+        proposal.json['project_id'] = proposal.project_id
+        proposal.json['show_project_id'] = False
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('document', '0011_auto_20170404_2058'),
+    ]
+
+    operations = [
+        RunInSchemas(migrations.RunPython(migrate_json)),
+    ]
