e6efb349eeda712386afe66e275e3ce89f0ec60d
Marius Räsener <mr@mehr-handwerk.de>
2017-04-07 14:14:56 +0200

    fix for fix :D

--- systori/apps/document/migrations/0012_fix_migrations_20170407.py
+++ systori/apps/document/migrations/0012_fix_migrations_20170407.py
@@ -11,6 +11,7 @@ def migrate_json(apps, schema_editor):
     for proposal in Proposal.objects.all():
         proposal.json['project_id'] = proposal.project_id
         proposal.json['show_project_id'] = False
+        proposal.save()
 
 
 class Migration(migrations.Migration):
