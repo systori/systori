ddfdfff1e5ca1a02d2b690d204a79e5cc733d443
Lex Berezhny <lex@damoti.com>
2017-07-27 20:46:05 -0400

    bericht based timesheet

--- systori/apps/document/type/timesheet.py
+++ systori/apps/document/type/timesheet.py
@@ -1,179 +1,72 @@
-from io import BytesIO
+import os
+import calendar
+from itertools import chain
 
-from reportlab.lib.units import mm
-from reportlab.platypus import Paragraph, Spacer, PageBreak
-from reportlab.lib import colors
+from django.conf import settings
+from django.utils.translation import ugettext as _
+from django.template.loader import get_template, render_to_string
 
-from django.utils.formats import date_format
-from django.utils.translation import pgettext_lazy, ugettext_lazy as _
+from bericht.pdf import PDFStreamer
+from bericht.html import HTMLParser, CSS
 
 from systori.apps.timetracking.models import Timer
-
-import calendar
-
-from .style import SystoriDocument, TableStyler
-from .style import LetterheadCanvas
-from .style import get_available_width_height_and_pagesize, b
-from .font import FontManager
-
-from systori.lib.templatetags.customformatting import zeroblank, hours, workdays
 from ..utils import get_weekday_names_numbers_and_mondays
 
-DEBUG_DOCUMENT = False  # Shows boxes in rendered output
 
+class TimesheetRenderer:
 
-CATEGORIES = [p[0] for p in Timer.KIND_CHOICES if p[0] != Timer.WORK]
+    def __init__(self, timesheets, letterhead):
+        self.timesheets = timesheets
+        self.letterhead = letterhead
 
+        # cache template lookups
+        self.timesheet_html = get_template('document/timesheet.html')
 
-def create_timesheet_table(json, available_width, font):
+    @property
+    def pdf(self):
+        return PDFStreamer(
+            HTMLParser(self.generate(), CSS(self.css)),
+            os.path.join(settings.MEDIA_ROOT, self.letterhead.letterhead_pdf.name),
+            'landscape'
+        )
 
-    start = json['first_weekday']
-    days = json['total_days']
-    names, numbers, mondays = get_weekday_names_numbers_and_mondays(start, days)
-    names.append(_("Total"))
+    @property
+    def html(self):
+        return ''.join(chain(
+            ('<style>', self.css, '</style>'),
+            self.generate()
+        ))
 
-    ts = TableStyler(font, base_style=False)
-    ts.style.append(('INNERGRID', (0, 0), (-1, -1), 0.25, colors.black))
-    ts.style.append(('LINEAFTER', (30, 0), (30, -1), 1.5, colors.black))
-    ts.style.append(('BOX', (0, 0), (-1, -1), 0.25, colors.black))
-    ts.style.append(('LEFTPADDING', (0, 0), (-2, -1), 2))
-    ts.style.append(('RIGHTPADDING', (0, 0), (-2, -1), 2))
-    ts.style.append(('ALIGN', (0, 2), (-2, -1), "RIGHT"))
-    ts.style.append(('VALIGN', (0, 2), (-2, -1), "MIDDLE"))
+    @property
+    def css(self):
+        return render_to_string('document/timesheet.css', {
+            'letterhead': self.letterhead
+        })
 
-    ts.style.append(('FONTSIZE', (0, 0), (-1, 1), 8))  # table header
-    ts.style.append(('FONTSIZE', (-1, 2), (-1, -1), 8))  # row type names
-    ts.style.append(('FONTSIZE', (0, 2), (-2, -1), 8))  # body
-
-    for monday in mondays:
-        ts.style.append(('LINEBEFORE', (monday, 0), (monday, -1), 1.5, colors.black))
-
-    ts.row(*numbers)
-    ts.row_style('ALIGNMENT', 0, -1, "CENTER")
-
-    ts.row(*names)
-    ts.row_style('ALIGNMENT', 0, -1, "CENTER")
-    ts.row_style('LINEBELOW', 0, -1, 1.25, colors.black)
-
-    def render_row(days, total, name):
-        columns = [""]*31 + [hours(zeroblank(total)), name]
-        for i, mins in enumerate(days):
-            columns[i] = hours(zeroblank(mins))
-        return columns
-
-    stripe_idx = 1
-
-    def stripe():
-        nonlocal stripe_idx
-        if stripe_idx % 2 == 0:
-            ts.row_style('BACKGROUND', 0, -1, colors.HexColor(0xCCFFFF))
-        stripe_idx += 1
-
-    lookup = dict(Timer.KIND_CHOICES)
-    lookup.update({
-        'payables': _('Total'),
-        'overtime': _('Overtime'),
-        'compensation': _('Compensation'),
-    })
-    row_types = [
-        'work', 'vacation', 'sick', 'public_holiday',
-        'paid_leave', 'unpaid_leave', 'payables',
-        'overtime', 'compensation',
-    ]
-    for row in row_types:
-        stripe()
-        ts.row(*render_row(json[row], json[row+'_total'], lookup[row]))
-        if row == 'payables':
-            ts.row_style('LINEABOVE', 0, -1, 1.5, colors.black)
-        if row == 'compensation':
-            ts.row_style('LINEABOVE', 0, -1, 1.5, colors.black)
-
-    max_days = 31
-    last_columns = [32, 65]  # total, row types
-    day_columns = [(available_width-sum(last_columns))/max_days]*max_days
-    return ts.get_table(colWidths=day_columns+last_columns, rowHeights=18)
-
-
-def create_rolling_balances(month, json, font):
-    ts = TableStyler(font, base_style=False)
-    ts.style.append(('GRID', (0, 0), (-1, -1), 0.25, colors.black))
-    ts.row("", pgettext_lazy("timesheet", "Previous"), pgettext_lazy("timesheet", "Correction"), month,
-           b(pgettext_lazy("timesheet", "Balance"), font))
-    ts.row(
-        _("Vacation"),
-        workdays(zeroblank(json['vacation_transferred'])),
-        workdays(zeroblank(json['vacation_correction'])),
-        "{}{}".format(
-            workdays(json['vacation_net']),
-            " ({} - {})".format(
-                workdays(json['vacation_added']),
-                workdays(json['vacation_total'])
-            ) if json['vacation_total'] != 0 else ""
-        ),
-        b(workdays(json['vacation_balance']), font)
-    )
-    ts.row(
-        _("Overtime"),
-        hours(zeroblank(json['overtime_transferred'])),
-        hours(zeroblank(json['overtime_correction'])),
-        "{}{}".format(
-            hours(json['overtime_net']),
-            " ({} - {})".format(
-                hours(json['overtime_total']),
-                hours(json['paid_leave_total'])
-            ) if json['paid_leave_total'] != 0 else ""
-        ),
-        b(hours(json['overtime_balance']), font)
-    )
-    ts.row(
-        _("Compensation"),
-        "",
-        hours(zeroblank(json['work_correction'])),
-        "{}{}".format(
-            hours(json['compensation_total']),
-            " ({} - {})".format(
-                hours(json['payables_total']),
-                hours(json['overtime_total'])
-            ) if json['overtime_total'] != 0 else ""
-        ),
-        b(hours(json['compensation_final']), font)
-    )
-    return ts.get_table(colWidths=[90]*3+[152]+[132], rowHeights=18, hAlign='RIGHT')
-
-
-def create_signature(json, font):
-    ts = TableStyler(font, base_style=False)
-    ts.row(pgettext_lazy("timesheet", "Approved") + ": ")
-    ts.style.append(('INNERGRID', (0, 0), (0, 1), 0.25, colors.black))
-
-    return ts.get_table(colWidths=[284], rowHeights=22, hAlign='RIGHT')
-
-
-def create_timesheets(timesheets, available_width, available_height, font):
-    for timesheet in timesheets:
-        json = timesheet.json
-        yield Paragraph('{}, {}, {}'.format(
-            json['last_name'],
-            json['first_name'],
-            date_format(timesheet.document_date, "F Y", use_l10n=True)),
-            font.h2)
-        yield Spacer(0, 4*mm)
-        yield create_timesheet_table(json, available_width, font)
-        yield Spacer(0, 4*mm)
-        yield create_rolling_balances(date_format(timesheet.document_date, "F", use_l10n=True), json, font)
-        yield Spacer(0, 4*mm)
-        yield create_signature(json, font)
-        yield PageBreak()
-
-
-def render(timesheets, letterhead):
-    with BytesIO() as buffer:
-        font = FontManager(letterhead.font)
-        available_width, available_height, pagesize = get_available_width_height_and_pagesize(letterhead)
-        doc = SystoriDocument(buffer, pagesize=pagesize, debug=DEBUG_DOCUMENT)
-        flowables = create_timesheets(timesheets, available_width, available_height, font)
-        doc.build(list(flowables), LetterheadCanvas.factory(letterhead), letterhead)
-        return buffer.getvalue()
+    def generate(self):
+        lookup = dict(Timer.KIND_CHOICES)
+        lookup.update({
+            'payables': _('Total'),
+            'overtime': _('Overtime'),
+            'compensation': _('Compensation'),
+        })
+        for timesheet in self.timesheets:
+            json = timesheet.json
+            context = {
+                'timesheet': timesheet,
+                'document_date': timesheet.document_date
+            }
+            context.update(json)
+            context['daynames'], context['daynumbers'], context['mondays'] = \
+                get_weekday_names_numbers_and_mondays(json['first_weekday'], json['total_days'], False)
+            context['rows'] = [
+                (t, lookup[t], json[t], json[t+'_total']) for t in [
+                    'work', 'vacation', 'sick', 'public_holiday',
+                    'paid_leave', 'unpaid_leave', 'payables',
+                    'overtime', 'compensation',
+                ]
+            ]
+            yield self.timesheet_html.render(context)
 
 
 class TimeSheetCollector:
