54c9b6af815e956f8e5095d45dea071d6806357b
Marius Räsener <mr@mehr-handwerk.de>
2017-08-21 16:06:15 +0200

    updated based on feedback

--- systori/static/js/day_based_overview.js
+++ systori/static/js/day_based_overview.js
@@ -1,49 +1,54 @@
-  $(document).ready(function(){
+$(document).ready(function () {
+    resizeRows();
+
     $("#progress").animate({
-       width: "0%"
-    }, 15000, function() {
-      page_reload();
-      }
+            width: "0%"
+        }, 15000, function () {
+            page_reload();
+        }
     );
+});
 
-  });
-
-  function page_reload()
-  {
+function page_reload() {
     $.ajax({
-      type: "HEAD",
-      url: window.location,
-      success: function() {
-        window.location.reload();
-      },
-      error: function() {
-        console.log("error reloading page.");
-        setTimeout(page_reload, 60000);
-      },
-      statusCode: {
-        404: function() { console.log("404"); },
-        501: function() { console.log("501"); },
-        502: function() { console.log("502"); }
-      }
+        type: "HEAD",
+        url: window.location,
+        success: function () {
+            window.location.reload();
+        },
+        error: function () {
+            console.log("error reloading page.");
+            setTimeout(page_reload, 60000);
+        },
+        statusCode: {
+            404: function () {
+                console.log("404");
+            },
+            501: function () {
+                console.log("501");
+            },
+            502: function () {
+                console.log("502");
+            }
+        }
     });
-  }
+}
 
-  function getAndSetMaxCellHeight(tag) {
+function getAndSetMaxCellHeight(tag) {
     let max = 0;
     const elements = document.querySelectorAll(tag);
-    for (const el of elements){
+    for (const el of elements) {
         if (el.offsetHeight > max)
             max = el.offsetHeight
     }
-    for (const el of elements){
+    for (const el of elements) {
         el.style.height = `${max}px`;
     }
-  }
+}
 
-  function resizeRows() {
-      for (const child of document.querySelector("grid-child").children) {
-          getAndSetMaxCellHeight(child.tagName);
-      }
-  }
+function resizeRows() {
+    for (const child of document.querySelector("grid-child").children) {
+        getAndSetMaxCellHeight(child.tagName);
+    }
+}
 
-$(resizeRows)
