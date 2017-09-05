df3f5018767daa75fc59dc7f645662582ba6f411
Lex Berezhny <lex@damoti.com>
2017-09-04 21:42:12 -0400

    celery autodiscover_tasks() no longer needs INSTALLED_APPS

--- systori/celery.py
+++ systori/celery.py
@@ -1,6 +1,5 @@
-import os
 from celery import Celery
 from django.conf import settings
 app = Celery('systori')
 app.config_from_object(settings)
-app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
+app.autodiscover_tasks()
