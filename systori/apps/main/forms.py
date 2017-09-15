ee2fdf42b11d23ea6765856d9b73649a27017e3c
Marius Räsener <mr@mehr-handwerk.de>
2017-09-15 12:43:28 +0200

    styling for notes. timeago and shortkey to submit

--- systori/apps/main/forms.py
+++ systori/apps/main/forms.py
@@ -1,4 +1,5 @@
 from django.forms import ModelForm
+from django.forms.widgets import Textarea
 from .models import Note
 
 
@@ -7,3 +8,9 @@ class NoteForm(ModelForm):
     class Meta:
         model = Note
         fields = 'text',
+        widgets = {
+            'text': Textarea(attrs={
+                'id': 'note-input',
+                'style': 'width: 100%; resize: none; border: 1px dotted grey;'
+            }),
+        }
