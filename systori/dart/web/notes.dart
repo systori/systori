9ebb0c98a78f4aae9afaa01c2d3424e8e628b75b
Marius Räsener <mr@mehr-handwerk.de>
2017-09-27 11:08:15 +0200

    now ready, forgot to cleanup in notes.dart

--- systori/dart/web/notes.dart
+++ systori/dart/web/notes.dart
@@ -7,89 +7,6 @@ String translated_save;
 String note_url = window.location.origin+"/api/note/";
 
 
-class NoteDeleteButton extends ButtonElement {
-    int note_pk;
-
-    NoteDeleteButton.created() : super.created();
-
-    void noteDelete(note_pk) {
-        String url = note_url + note_pk.toString();
-        HttpRequest.request(url, method: 'delete', requestHeaders: {"X-CSRFToken": CSRFToken}).then(removeFromDom);
-    }
-
-    void removeFromDom(var response) {
-        if (response.status == 204) {
-            document.querySelector("tr[data-note-pk='$note_pk']").remove();
-        } else {
-            print('false');
-        }
-    }
-}
-
-
-class NoteEditButton extends ButtonElement {
-    Map note;
-    int get pk => int.parse(this.closest("tr").dataset['pk']);
-    SpanElement get note_span => this.parent.parent.querySelector("span");
-    TextAreaElement get textarea => this.parent.parent.querySelector("textarea");
-    DivElement get btngrp => this.parent;
-    ButtonElement get save_btn => this.parent.parent.querySelector('#note-save-button');
-
-    NoteEditButton.created() : super.created();
-
-    @override
-    attached() {
-        this.onClick.listen((_) {
-            noteEdit();
-        });
-    }
-
-    void noteEdit() {
-        [note_span, textarea, btngrp, save_btn].forEach((el) {
-            el.classes.toggle("hidden");
-        });
-    }
-}
-
-
-class NoteSaveButton extends ButtonElement {
-
-    int get pk => int.parse(this.closest("tr").dataset['pk']);
-    NoteSaveButton.created(): super.created();
-
-    @override
-    attached() {
-        this.onClick.listen((_) {
-            noteSave(this, pk);
-        });
-//        this.parent.querySelector("textarea").addEventListener("keypress", (dynamic e) {
-//            if (e.shiftKey && (e.keyCode == 13)) {
-//                e.preventDefault();
-//                noteSave(this, pk);
-//            }
-//        });
-    }
-
-    void noteSave(NoteSaveButton instance, int pk) {
-        String url = note_url + pk.toString();
-        TextAreaElement textarea = this.parent.querySelector("textarea");
-        HttpRequest.request(url,
-                method: 'put',
-                sendData: JSON.encode({'text':textarea.value}),
-                requestHeaders: {
-                    "X-CSRFToken": CSRFToken,
-                    'Content-Type': 'application/json',
-                    'Accept': 'application/json'
-                    }).then((_) => HttpRequest.request(url, method: 'get').then((response) => noteUpdate(instance, response)));
-    }
-
-    void noteUpdate(NoteSaveButton instance, response) {
-        if (response.status == 200) {
-            var data = JSON.decode(response.responseText);
-        }
-    }
-}
-
 class NoteTableRow extends TableRowElement {
     int get pk => int.parse(dataset['pk']);
     int get worker_pk => int.parse(dataset['workerPk']);
@@ -170,7 +87,6 @@ class NoteTableRow extends TableRowElement {
 
 void main() {
     CSRFToken = (querySelector('input[name=csrfmiddlewaretoken]') as InputElement).value;
-    translated_save = querySelector('#translated_save').text;
     document.registerElement('note-tr', NoteTableRow, extendsTag: 'tr');
 
     document.querySelector('#note-input').onKeyPress.listen((e) {
