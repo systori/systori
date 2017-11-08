06df9f6155190671bbd0c51e8c9cc44869759e40
Lex Berezhny <lex@damoti.com>
2017-11-08 18:14:33 -0500

    company creation self-service

--- systori/apps/company/forms.py
+++ systori/apps/company/forms.py
@@ -1,8 +1,46 @@
+from django.utils.translation import ugettext_lazy as _
+
+from systori.apps.project.models import Project
+from systori.apps.accounting.workflow import create_chart_of_accounts
+
 from django.forms import ModelForm
-from .models import Company
+from .models import Company, Worker
 
 
 class CompanyForm(ModelForm):
     class Meta:
         model = Company
-        fields = '__all__'
+        fields = 'name', 'schema', 'timezone', 'is_jobsite_required'
+        labels = {
+            'schema': _('Sub-Domain'),
+        }
+        help_texts = {
+            'name': _('Full legal name of your company.'),
+            'schema': _(
+                'Short and simple version of your company name used in URLs. '
+                'May only contain lowercase letters, digits and underscores. '
+                'Must start with a letter. Cannot be changed later.')
+        }
+
+    def __init__(self, *args, user=None, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.user = user
+        if self.instance.schema:
+            self.fields['schema'].disabled = True
+
+    def save(self, commit=True):
+        adding = self.instance._state.adding
+        company = super().save(commit)
+        if adding:
+            company.activate()
+            Worker.objects.create(
+                company=company,
+                user=self.user,
+                is_owner=True
+            )
+            Project.objects.create(
+                name="Template Project",
+                is_template=True
+            )
+            create_chart_of_accounts()
+        return company
--- systori/apps/company/views.py
+++ systori/apps/company/views.py
@@ -1,17 +1,22 @@
-from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+from django.views.generic import TemplateView, CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 from .models import Company
 from .forms import CompanyForm
 
 
-class CompanyList(ListView):
-    model = Company
+class CompanyList(TemplateView):
+    template_name = 'company/company_list.html'
 
 
 class CompanyCreate(CreateView):
     model = Company
     form_class = CompanyForm
     success_url = reverse_lazy('companies')
+
+    def get_form_kwargs(self):
+        kwargs = super().get_form_kwargs()
+        kwargs['user'] = self.request.user
+        return kwargs
 
 
 class CompanyUpdate(UpdateView):
