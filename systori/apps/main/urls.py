b54a9bf92882892f2e094a73a75e8ecbf93a09b0
Marius Räsener <mr@mehr-handwerk.de>
2017-12-05 15:31:34 +0100

    hotfix for security issue about NotesDashboard - no login was required

--- systori/apps/main/urls.py
+++ systori/apps/main/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     url(r'^$', IndexView.as_view(), name='home'),
     url(r'^day-based-overview/(?P<selected_day>\d{4}-\d{2}-\d{2})?$', office_auth(DayBasedOverviewView.as_view()),
         name='day_based_overview'),
-    url(r'^notes$', NotesDashboard.as_view(), name='notes'),
+    url(r'^notes$', office_auth(NotesDashboard.as_view()), name='notes'),
 ]
