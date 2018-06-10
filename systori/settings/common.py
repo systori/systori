8d35739c07797b146da3bae4bac726e7d2b6f1ac
Marius Räsener <m.raesener@gmail.com>
2018-06-10 23:38:34 +0200

    fixing settings

--- systori/settings/common.py
+++ systori/settings/common.py
@@ -151,6 +151,17 @@ DATABASES = {
         'ENGINE': 'systori.db',
     }
 }
+if 'TRAVIS' in os.environ:
+    DATABASES = {
+        'default': {
+            'ENGINE':   'django.db.backends.postgresql_psycopg2',
+            'NAME':     'travisci',
+            'USER':     'postgres',
+            'PASSWORD': '',
+            'HOST':     'localhost',
+            'PORT':     '',
+        }
+}
 
 
 AUTHENTICATION_BACKENDS = (
