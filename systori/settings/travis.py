c369ae0b280ec049831684806a32516050d37ac2
Marius Räsener <m.raesener@gmail.com>
2018-06-11 17:04:18 +0200

    different staticfiles path for dart, now used from common.py

--- systori/settings/travis.py
+++ systori/settings/travis.py
@@ -7,25 +7,7 @@ SERVER_NAME = 'systori.localhost'
 SESSION_COOKIE_DOMAIN = '.'+SERVER_NAME
 ALLOWED_HOSTS = ['.'+SERVER_NAME]
 
-STATICFILES_DIRS += (
-    ('dart', 'systori/dart/build/web'),
-)
-
 MEDIA_ROOT = os.path.normpath(os.path.join(ROOT_DIR, 'media'))
-
-# DATABASES['default'].update(
-#         {
-#         'ENGINE':   'django.db.backends.postgresql_psycopg2',
-#         'NAME':     'travisci',
-#         'USER':     'postgres',
-#         'PASSWORD': '',
-#         'HOST':     'localhost',
-#         'PORT':     '',
-#         'TEST': {
-#         'SERIALIZE': False
-#         }
-#     }
-# )
 
 PASSWORD_HASHERS = (
     'django.contrib.auth.hashers.MD5PasswordHasher',
