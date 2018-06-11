2342f7165e2c9caa879020e364055bf0cd1b176a
Marius Räsener <m.raesener@gmail.com>
2018-06-11 12:42:36 +0200

    next try updated db settings

--- systori/settings/travis.py
+++ systori/settings/travis.py
@@ -13,19 +13,19 @@ STATICFILES_DIRS += (
 
 MEDIA_ROOT = os.path.normpath(os.path.join(ROOT_DIR, 'media'))
 
-DATABASES['default'].update(
-        {
-        'ENGINE':   'django.db.backends.postgresql_psycopg2',
-        'NAME':     'travisci',
-        'USER':     'postgres',
-        'PASSWORD': '',
-        'HOST':     'localhost',
-        'PORT':     '',
-        'TEST': {
-        'SERIALIZE': False
-        }
-    }
-)
+# DATABASES['default'].update(
+#         {
+#         'ENGINE':   'django.db.backends.postgresql_psycopg2',
+#         'NAME':     'travisci',
+#         'USER':     'postgres',
+#         'PASSWORD': '',
+#         'HOST':     'localhost',
+#         'PORT':     '',
+#         'TEST': {
+#         'SERIALIZE': False
+#         }
+#     }
+# )
 
 PASSWORD_HASHERS = (
     'django.contrib.auth.hashers.MD5PasswordHasher',
