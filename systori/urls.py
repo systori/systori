38b01b2427638a84999ccf4987e006c49338c0a4
Marius Räsener <marius@raesener.de>
2019-04-23 12:42:20 +0200

    drf-yasg working

--- systori/urls.py
+++ systori/urls.py
@@ -2,7 +2,23 @@ from django.conf.urls import include, url
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.views.static import serve
 from django.conf import settings
-from rest_framework.documentation import include_docs_urls
+
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Systori API",
+        default_version="v0.1",
+        description="API Endpoints for Systori",
+        contact=openapi.Contact(email="support@systori.com"),
+        license=openapi.License(name="MIT License"),
+    ),
+    public=True,
+    permission_classes=(permissions.IsAuthenticated,),
+)
+
 
 urlpatterns = [
     url(r"^media/(?P<path>.*)$", serve, {"document_root": settings.MEDIA_ROOT}),
@@ -16,7 +32,6 @@ urlpatterns = [
     url(r"^api/equipment/", include("systori.apps.equipment.routers")),
     url(r"^api/main/", include("systori.apps.main.routers")),
     url(r"^api/timetracking/", include("systori.apps.timetracking.routers")),
-    url(r"^api/docs/", include_docs_urls(title="Systori REST API")),
     url(r"^i18n/", include("django.conf.urls.i18n")),
     url(r"", include("systori.apps.company.urls")),
     url(r"", include("systori.apps.project.urls")),
@@ -30,12 +45,25 @@ urlpatterns = [
     url(r"^project-(?P<project_pk>\d+)/", include("systori.apps.task.urls")),
     url(r"^templates/", include("systori.apps.task.urls")),
     url(r"^field/", include("systori.apps.field.urls")),
+    url(
+        r"^swagger(?P<format>\.json|\.yaml)$",
+        schema_view.without_ui(cache_timeout=0),
+        name="schema-json",
+    ),
+    url(
+        r"^swagger/$",
+        schema_view.with_ui("swagger", cache_timeout=0),
+        name="schema-swagger-ui",
+    ),
+    url(
+        r"^redoc/$", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"
+    ),
 ]
 
 urlpatterns += staticfiles_urlpatterns()
 
 
-if settings.ENABLE_DEBUGTOOLBAR:
+if settings.DEBUG:
     import debug_toolbar
 
     urlpatterns = [url(r"^__debug__/", include(debug_toolbar.urls))] + urlpatterns
