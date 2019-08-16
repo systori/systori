966a396a0871bce51f30a5cbb2e3e24f3f180d6a
devkabiir <dev@kabiir.me>
2019-08-17 02:32:31 +0530

    💡docs (user) Explain `HasLaborerAccess` permission

--- systori/apps/user/permissions.py
+++ systori/apps/user/permissions.py
@@ -33,6 +33,10 @@ class HasStaffAccess(WorkerIsAuthenticated):
 
 
 class HasLaborerAccess(WorkerIsAuthenticated):
+    """
+    Limited access in the field.
+    """
+
     def has_permission(self, request, view):
         is_authenticated = super().has_permission(request, view)
         return is_authenticated and request.worker.has_laborer
