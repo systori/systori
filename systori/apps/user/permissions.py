67f008f8de72756d111b272efcf835ca8baa6564
devkabiir <dev@kabiir.me>
2019-08-17 02:34:21 +0530

    💡docs (user) Explain `HasOwnerAccess` permission

--- systori/apps/user/permissions.py
+++ systori/apps/user/permissions.py
@@ -53,6 +53,10 @@ class HasForemanAccess(WorkerIsAuthenticated):
 
 
 class HasOwnerAccess(WorkerIsAuthenticated):
+    """
+    Has full and unlimited access.
+    """
+
     def has_permission(self, request, view):
         is_authenticated = super().has_permission(request, view)
         return is_authenticated and request.worker.has_owner
