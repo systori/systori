9cfd0f234bbcb601ea0180414345cd1f938d4988
devkabiir <dev@kabiir.me>
2019-08-17 02:31:37 +0530

    💡docs (user) Explain `HasStaffAccess` permission

--- systori/apps/user/permissions.py
+++ systori/apps/user/permissions.py
@@ -23,6 +23,10 @@ class WorkerIsAuthenticated(permissions.IsAuthenticated):
 
 
 class HasStaffAccess(WorkerIsAuthenticated):
+    """
+    Manage projects and perform administrative tasks.
+    """
+
     def has_permission(self, request, view):
         is_authenticated = super().has_permission(request, view)
         return is_authenticated and request.worker.has_staff
