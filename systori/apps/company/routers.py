eff952ce01c7e865c38a49f3fa087c1442a9c9fb
devkabiir <dev@kabiir.me>
2019-08-17 22:33:15 +0530

    fixup! ✨feat (company) Add `Worker` model endpoints

--- systori/apps/company/routers.py
+++ systori/apps/company/routers.py
@@ -4,6 +4,6 @@ from systori.apps.company.api import CompanyModelViewSet, WorkerModelViewSet
 
 router = SimpleRouter()
 router.register(r"company", CompanyModelViewSet)
-router.register(r"company/worker", WorkerModelViewSet)
+router.register(r"worker", WorkerModelViewSet)
 
 urlpatterns = router.urls
