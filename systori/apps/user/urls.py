b98597c350e8b99a2bcbc26a1a648923884914cd
Marius Räsener <marius@raesener.de>
2019-08-29 19:15:37 +0200

    minor clean up

--- systori/apps/user/urls.py
+++ systori/apps/user/urls.py
@@ -1,30 +1,38 @@
-from ..user.authorization import office_auth
 from django.contrib.auth.decorators import login_required
 from django.conf.urls import url, include
 from rest_framework.authtoken.views import ObtainAuthToken
-from . import views
+
+from systori.apps.user.authorization import office_auth
+from systori.apps.user.views import (
+    SettingsView,
+    SetLanguageView,
+    WorkerList,
+    UserAdd,
+    UserView,
+    UserUpdate,
+    WorkerRemove,
+    UserGeneratePassword,
+)
 
 urlpatterns = [
     url(r"^accounts/", include("allauth.urls")),
     url(r"api/token/", ObtainAuthToken.as_view(), name="drf.tokenauth"),
-    url(r"^settings$", login_required(views.SettingsView.as_view()), name="settings"),
-    url(r"^set_language$", views.SetLanguageView.as_view(), name="set_language"),
-    url(r"^users$", office_auth(views.WorkerList.as_view()), name="users"),
-    url(r"^add-user$", office_auth(views.UserAdd.as_view()), name="user.add"),
-    url(r"^user-(?P<pk>\d+)$", office_auth(views.UserView.as_view()), name="user.view"),
+    url(r"^settings$", login_required(SettingsView.as_view()), name="settings"),
+    url(r"^set_language$", SetLanguageView.as_view(), name="set_language"),
+    url(r"^users$", office_auth(WorkerList.as_view()), name="users"),
+    url(r"^add-user$", office_auth(UserAdd.as_view()), name="user.add"),
+    url(r"^user-(?P<pk>\d+)$", office_auth(UserView.as_view()), name="user.view"),
     url(
-        r"^user-(?P<pk>\d+)/edit$",
-        office_auth(views.UserUpdate.as_view()),
-        name="user.edit",
+        r"^user-(?P<pk>\d+)/edit$", office_auth(UserUpdate.as_view()), name="user.edit"
     ),
     url(
         r"^access-(?P<pk>\d+)/remove$",
-        office_auth(views.WorkerRemove.as_view()),
+        office_auth(WorkerRemove.as_view()),
         name="access.remove",
     ),
     url(
         r"^user-(?P<pk>\d+)/generate-password$",
-        office_auth(views.UserGeneratePassword.as_view()),
+        office_auth(UserGeneratePassword.as_view()),
         name="user.generate.password",
     ),
 ]
